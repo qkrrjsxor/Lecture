1d000e6d8135527c12474f4f5d8541415a2b6e99
qkrrjsxor <qkrrjsxor12@gmail.com>
2024-05-09 18:07:06 +0900

    5.9

---
+++ Vue.js/0509_day09_SpringBoot_VueJS/사전제공코드/board/BoardDetail.vue
@@ -0,0 +1,10 @@
+<template>
+    <div>
+        <h4>게시글 상세</h4>
+    </div>
+</template>
+
+<script setup>
+</script>
+
+<style scoped></style>
---
+++ Vue.js/0509_day09_SpringBoot_VueJS/사전제공코드/board/BoardList.vue
@@ -0,0 +1,11 @@
+<template>
+    <div>
+        <h4>게시글 목록</h4>
+    </div>
+</template>
+
+<script setup>
+
+</script>
+
+<style scoped></style>
