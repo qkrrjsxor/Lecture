abd618279564bc7a6c59b8674355750761c66a56
qkrrjsxor <qkrrjsxor12@gmail.com>
2024-05-10 18:02:02 +0900

    05.10

---
+++ Vue.js/0510_day10_API/vue-api-project/src/components/youtube/YoutubeVideoList.vue
@@ -0,0 +1,21 @@
+<template>
+  <div>
+    <h4>검색결과</h4>
+    <ul>
+      <YoutubeVideoListItem
+        v-for="video in store.videos"
+        :key="video.id.videoId"
+        :video="video"
+      ></YoutubeVideoListItem>
+    </ul>
+  </div>
+</template>
+
+<script setup>
+import { useYoutubeStore } from '@/stores/youtube';
+import YoutubeVideoListItem from './YoutubeVideoListItem.vue';
+
+const store = useYoutubeStore();
+</script>
+
+<style scoped></style>
---
+++ Vue.js/0510_day10_API/vue-api-project/src/views/HomeView.vue
@@ -0,0 +1,17 @@
+<template>
+  <div>
+    <h2>HomeView</h2>
+    <hr>
+    <WeatherForecast/>
+  </div>
+</template>
+
+<script setup>
+import WeatherForecast from '@/components/weather/WeatherForecast.vue';
+
+
+</script>
+
+<style scoped>
+
+</style>
